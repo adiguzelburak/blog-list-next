2951ade363ce36c25799114fcc8edf5b67d57db1
unknown <adiguzelburak@icloud.com>
2023-07-10 09:43:23 +0300

    blog list added.

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,6 @@ type Props = {
   postInfo: PostInfoType;
 }
 
-
 export default function Home({ postInfo }: Props) {
   const dispatch = useDispatch();
   const [isSearched, setIsSearched] = useState(false)
@@ -40,6 +39,13 @@ export default function Home({ postInfo }: Props) {
     setSearchedTitle(e.target.value)
   }
 
+  const summary = (text: string) => {
+    const sliceText = text.slice(0, 50);
+    const summaryText = sliceText + (text.length > 50 ? "..." : "");
+
+    return summaryText;
+  };
+
   return (
     <>
       <Head>
@@ -49,22 +55,55 @@ export default function Home({ postInfo }: Props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <h1>Blog List</h1>
+        <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+          <div className="absolute inset-0">
+            <div className="bg-white h-1/3 sm:h-2/3" />
+          </div>
+          <div className="relative max-w-7xl mx-auto">
+            <div className="text-center">
+              <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Blog List</h2>
+              <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+                Welcome to my Blog List
+              </p>
+            </div>
+            <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+              {isSearched
+                ? <>
+                  {searched?.posts?.map(((post: PostType) =>
+                    <Link href={`/posts/${post.id}`} key={post.id} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                      <div className="flex-shrink-0">
+                        <img className="h-48 w-full object-cover" src={'https://loremflickr.com/640/480/abstract'} alt="ds" />
+                      </div>
+                      <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                        <div className="flex-1">
+                          <div className="block mt-2">
+                            <p className="text-xl font-semibold text-gray-900">{post.title}</p>
+                            <p className="mt-3 text-base text-gray-500">{summary(post.body)}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>))}</>
+                : <>
+                  {posts.map(((post: PostType) =>
+                    <Link href={`/posts/${post.id}`} key={post.id} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                      <div className="flex-shrink-0">
+                        <img className="h-48 w-full object-cover" src={'https://loremflickr.com/640/480/abstract'} alt="ds" />
+                      </div>
+                      <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                        <div className="flex-1">
+                          <div className="block mt-2">
+                            <p className="text-xl font-semibold text-gray-900">{post.title}</p>
+                            <p className="mt-3 text-base text-gray-500">{summary(post.body)}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>))}</>}
+            </div>
+          </div>
+        </div>
         <input
           onChange={onChange}
           placeholder='Please enter a Post Name' />
-
-        <ul>
-          {isSearched
-            ? <>
-              {searched?.posts?.map(((post: PostType) => <li key={post.id}>
-                <Link href={`/posts/${post.id}`}>{post.title}</Link>
-              </li>))}</>
-            : <>
-              {posts.map(((post: PostType) => <li key={post.id}>
-                <Link href={`/posts/${post.id}`}>{post.title}</Link>
-              </li>))}</>}
-        </ul>
       </div >
     </>
   )
